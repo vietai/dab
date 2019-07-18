25fbb4613d2940d1e5dae77d04654628e911b074
thtrieu <thtrieu@apcs.vn>
2019-07-18 07:06:55 +0700

    Create t2t_decoder.py

---
+++ t2t_decoder.py
@@ -0,0 +1,15 @@
+"""Decode."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+from tensor2tensor.bin import t2t_decoder
+from tensor2tensor.models import transformer
+import problems
+import tensorflow as tf
+
+
+if __name__ == '__main__':
+  tf.logging.set_verbosity(tf.logging.INFO)
+  tf.app.run(t2t_decoder.main)
