e3ff7032232229dfe91d93e2decb4a40cafed318
thtrieu <thtrieu@apcs.vn>
2019-08-13 09:25:47 +0700

    Accept other languages besides En and Vi

--- back_translate.py
+++ back_translate.py
@@ -16,27 +16,27 @@ import os
 registry = problems.registry
 
 tf.flags.DEFINE_string(
-    'vien_problem', 
+    'from_problem', 
     'translate_vien_iwslt32k', 
     'Problem name for Vietnamese to English translation.')
 tf.flags.DEFINE_string(
-    'envi_problem', 
+    'to_problem', 
     'translate_envi_iwslt32k', 
     'Problem name for English to Vietnamese translation.')
 tf.flags.DEFINE_string(
-    'vien_data_dir', 
+    'from_data_dir', 
     'gs://vien-translation/data/translate_vien_iwslt32k', 
     'Data directory for Vietnamese to English translation.')
 tf.flags.DEFINE_string(
-    'envi_data_dir', 
+    'to_data_dir', 
     'gs://vien-translation/data/translate_envi_iwslt32k', 
     'Data directory for English to Vietnamese translation.')
 tf.flags.DEFINE_string(
-    'vien_ckpt', 
+    'from_ckpt', 
     'gs://vien-translation/checkpoints/translate_vien_iwslt32k_tiny/avg/', 
     'Pretrain checkpoint directory for Vietnamese to English translation.')
 tf.flags.DEFINE_string(
-    'envi_ckpt', 
+    'to_ckpt', 
     'gs://vien-translation/checkpoints/translate_envi_iwslt32k_tiny/avg/', 
     'Pretrain checkpoint directory for English to Vietnamese translation.')
 tf.flags.DEFINE_string(
@@ -52,8 +52,6 @@ tf.flags.DEFINE_boolean(
     False,
     'Whether to back-translate interactively.')
 
-tf.flags.DEFINE_string('lang', 'vi', 'Language of the input text file (vi or en).')
-
 FLAGS = tf.flags.FLAGS
   
 
@@ -61,31 +59,17 @@ FLAGS = tf.flags.FLAGS
 if __name__ == '__main__':
   tf.logging.set_verbosity(tf.logging.INFO)
 
-  # Set up some decoding flags depending on the input text language.
-  if FLAGS.lang == 'vi':
-    from_data_dir, to_data_dir = FLAGS.vien_data_dir, FLAGS.envi_data_dir
-    from_problem, to_problem = FLAGS.vien_problem, FLAGS.envi_problem
-    from_ckpt, to_ckpt = FLAGS.vien_ckpt, FLAGS.envi_ckpt
-    proxy_lang = 'en'
-  elif FLAGS.lang == 'en':
-    from_data_dir, to_data_dir = FLAGS.envi_data_dir, FLAGS.vien_data_dir
-    from_problem, to_problem = FLAGS.envi_problem, FLAGS.vien_problem
-    from_ckpt, to_ckpt = FLAGS.envi_ckpt, FLAGS.vien_ckpt
-    proxy_lang = 'vi'
-  else:
-    raise ValueError('Not supported language: {}'.format(lang))
-
   # Convert directory into checkpoints
-  if tf.gfile.IsDirectory(from_ckpt):
-    from_ckpt = tf.train.latest_checkpoint(from_ckpt)
-  if tf.gfile.IsDirectory(to_ckpt):
-    to_ckpt = tf.train.latest_checkpoint(to_ckpt)
+  if tf.gfile.IsDirectory(FLAGS.from_ckpt):
+    from_ckpt = tf.train.latest_checkpoint(FLAGS.from_ckpt)
+  if tf.gfile.IsDirectory(FLAGS.to_ckpt):
+    to_ckpt = tf.train.latest_checkpoint(FLAGS.to_ckpt)
 
   if FLAGS.backtranslate_interactively:
     decoding.backtranslate_interactively(
-        from_problem, to_problem,
-        from_data_dir, to_data_dir,
-        from_ckpt, to_ckpt)
+        FLAGS.from_problem, FLAGS.to_problem,
+        FLAGS.from_data_dir, FLAGS.to_data_dir,
+        FLAGS.from_ckpt, FLAGS.to_ckpt)
   else:
     # For back translation from file, we need a temporary file in the other language
     # before back-translating into the source language.
@@ -94,12 +78,11 @@ if __name__ == '__main__':
     )
 
     # Step 1: Translating from source language to the other language.
-    if not tf.gfile.Exists(tmp_file):
-      decoding.t2t_decoder(from_problem, from_data_dir,
-                           FLAGS.paraphrase_from_file, tmp_file,
-                           from_ckpt)
+    decoding.t2t_decoder(FLAGS.from_problem, FLAGS.from_data_dir,
+                         FLAGS.paraphrase_from_file, tmp_file,
+                         FLAGS.from_ckpt)
 
     # Step 2: Translating from the other language (tmp_file) to source.
-    decoding.t2t_decoder(to_problem, to_data_dir,
+    decoding.t2t_decoder(FLAGS.to_problem, FLAGS.to_data_dir,
                          tmp_file, FLAGS.paraphrase_to_file,
-                         to_ckpt)
+                         FLAGS.to_ckpt)
